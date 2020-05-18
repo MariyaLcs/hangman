b184ad110a04560ecc67c7818b5190bb960cf92f
Mariya <mariya.podosinova@gmail.com>
2020-05-18 13:07:08 +0100

    Updates

---
+++ precache-manifest.7912b89e877c3cd64c03714c7e1cc9f5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb74ba42b64a5f3a0ad8cff77f6937b",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "005d54d1ea3d8309406c",
+    "url": "/hangman/static/css/main.b9e14a6b.chunk.css"
+  },
+  {
+    "revision": "32ca68c0c6d88ddf5e41",
+    "url": "/hangman/static/js/2.75c9e4e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman/static/js/2.75c9e4e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "005d54d1ea3d8309406c",
+    "url": "/hangman/static/js/main.38c4496a.chunk.js"
+  },
+  {
+    "revision": "0bebc4792bb334fb65e4",
+    "url": "/hangman/static/js/runtime-main.ee16e613.js"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/hangman/static/media/0.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/hangman/static/media/1.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/hangman/static/media/2.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/hangman/static/media/3.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/hangman/static/media/4.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/hangman/static/media/5.1b557aa7.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/hangman/static/media/6.c80394c2.jpg"
+  }
+]);
